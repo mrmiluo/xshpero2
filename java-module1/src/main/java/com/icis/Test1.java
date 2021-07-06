6807dfe4072b7b48fc830c6a02d98230f33e5c3c
mrmiluo <2120131995@qq.com>
2021-07-06 14:58:21 +0800

    创建第一个模块

---
+++ java-module1/src/main/java/com/icis/Test1.java
@@ -0,0 +1,11 @@
+package com.icis;
+
+/**
+ * @author LYQ
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("HeHe...");
+        System.out.println("Hello git...");
+    }
+}
